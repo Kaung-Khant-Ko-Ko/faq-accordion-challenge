e7fb55fb5a9fe9a1dc8876cd35199c794c849ed0
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2024-05-06 21:09:51 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+$(".faq-a").slideUp(0);
+$("#faq-a-1").slideDown(0);
+
+$("#faq-q-1").on("click", function () {
+  var item = $("#faq-a-1");
+  if (item.is(":hidden")) {
+    item.slideDown();
+    $("#faq-q-1").attr("src", "./assets/images/icon-minus.svg");
+  } else {
+    item.slideUp();
+    $("#faq-q-1").attr("src", "./assets/images/icon-plus.svg");
+  }
+});
+
+$("#faq-q-2").on("click", function () {
+  var item = $("#faq-a-2");
+  if (item.is(":hidden")) {
+    item.slideDown();
+    $("#faq-q-2").attr("src", "./assets/images/icon-minus.svg");
+  } else {
+    item.slideUp();
+    $("#faq-q-2").attr("src", "./assets/images/icon-plus.svg");
+  }
+});
+
+$("#faq-q-3").on("click", function () {
+  var item = $("#faq-a-3");
+  if (item.is(":hidden")) {
+    item.slideDown();
+    $("#faq-q-3").attr("src", "./assets/images/icon-minus.svg");
+  } else {
+    item.slideUp();
+    $("#faq-q-3").attr("src", "./assets/images/icon-plus.svg");
+  }
+});
+
+$("#faq-q-4").on("click", function () {
+  var item = $("#faq-a-4");
+  if (item.is(":hidden")) {
+    item.slideDown();
+    $("#faq-q-4").attr("src", "./assets/images/icon-minus.svg");
+  } else {
+    item.slideUp();
+    $("#faq-q-4").attr("src", "./assets/images/icon-plus.svg");
+  }
+});
+
+$(".faq-q > h3").hover(
+  function () {
+    $(this).addClass("change-color");
+    console.log(this);
+  },
+  function () {
+    $(this).removeClass("change-color");
+    console.log(this);
+  }
+);
